cc7ab2d5d41d2d0657be457f9470497cdf489b3b
Denis Kolesnikov <denis3753@yandex.ru>
2021-03-08 22:02:42 +0300

    first commit in develop

--- homework-git.js
+++ homework-git.js
@@ -1,3 +1,9 @@
 //Fisrt practice with Git
 let hello = privet => console.log('first practice with Git');
 hello();
+
+//first commit in develop
+function firstCommitDevelop(){
+    console.log('first commit in develop');
+}
+firstCommitDevelop();
